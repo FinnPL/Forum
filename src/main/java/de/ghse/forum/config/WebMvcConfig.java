64e80e0379f0ddee58508a9ea3684dc7455b7c7a
schillD <davis.schill04@gmail.com>
2022-11-14 18:58:07 +0100

    CORS Fix

--- src/main/java/de/ghse/forum/config/WebMvcConfig.java
+++ src/main/java/de/ghse/forum/config/WebMvcConfig.java
@@ -1,13 +1,15 @@
 package de.ghse.forum.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true);
+    registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true);
   }
 }
 
