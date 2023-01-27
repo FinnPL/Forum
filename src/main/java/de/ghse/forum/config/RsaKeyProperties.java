4af0ea427562a3be5e34c808a877b80c5bf83503
finnp <contact@lippok.eu>
2023-01-27 21:54:53 +0100

    working

---
+++ src/main/java/de/ghse/forum/config/RsaKeyProperties.java
@@ -0,0 +1,11 @@
+package de.ghse.forum.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+}
