8e0b01ee629151441ec3934a08713e29986fdbe5
finnp <contact@lippok.eu>
2023-01-27 17:13:51 +0100

    works :)

--- src/main/java/de/ghse/forum/repository/PostRepository.java
+++ src/main/java/de/ghse/forum/repository/PostRepository.java
@@ -10,70 +10,22 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- *
- * <pre>
- * Post Repository Interface extends JpaRepository
- * </pre>
- *
- * @version 1.0
- * @since 1.0
- */
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
 
-  List<Post> findAllByDateOrderByDateDesc(Timestamp date, Pageable pageable);
+  @Query("SELECT p FROM Post p ORDER BY p.date DESC")
+  List<Post> getNewestPostByPage( Pageable pageable);
 
+  @Query("SELECT p FROM Post p WHERE p.user = ?1 ORDER BY p.date DESC")
+  List<Post> findPostsByUserByPage(User user, Pageable pageable);
 
-  /**
-   *
-   *
-   * <pre>
-   * Find all Posts by Title Containing a String
-   * </pre>
-   *
-   * @param title String to search for in Title
-   * @return List of Posts
-   * @see User
-   * @see Post
-   * @since 1.0
-   */
-  List<Post> findAllByTitleContaining(String title);
-
-  /**
-   *
-   *
-   * <pre>
-   * Find all Posts by User
-   * </pre>
-   *
-   * @param user User to search for
-   * @return List of Posts
-   * @see User
-   * @see Post
-   * @since 1.0
-   */
-  List<Post> findAllByUser(User user);
-
-  /**
-   *
-   *
-   * <pre>
-   * Find 20 posts by Title or Content containing a String
-   * </pre>
-   *
-   * @param title String to search for in Title
-   * @param content String to search for in Content
-   * @return List of Posts (max 20)
-   * @see Post
-   * @since 1.0
-   */
-  List<Post> findTop20ByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrderByDateDesc(
-      String title, String content);
+  //search for posts by title or content containing a string ignore-case and order by date descending
+  @Query ("SELECT p FROM Post p WHERE LOWER(p.title) LIKE LOWER(CONCAT('%',?1,'%')) OR LOWER(p.content) LIKE LOWER(CONCAT('%',?1,'%')) ORDER BY p.date DESC")
+  List<Post> search(String query, Pageable pageable);
 
 
 }
