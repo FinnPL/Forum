5f453a2201c8bccff3e5ca38e68f451fdc832717
finnp <contact@lippok.eu>
2023-01-27 17:32:09 +0100

    Paged user Search

--- src/main/java/de/ghse/forum/service/UserService.java
+++ src/main/java/de/ghse/forum/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,69 +22,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
+  private final int PAGE_SIZE = 3;
   @Autowired private UserRepository UserRepository;
 
-  /**
-   * Add User to Database
-   *
-   * @param user User to add
-   * @see User
-   * @see UserRepository
-   * @since 1.0
-   */
   public void addUser(User user) {
     UserRepository.save(user);
   }
 
-  /**
-   * Find User by ID
-   *
-   * @param id UUID of User
-   * @return Optional User
-   * @see User
-   * @see UserRepository
-   * @since 1.0
-   */
   public @NotNull Optional<User> findUserById(UUID id) {
     return UserRepository.findById(id);
   }
 
-  /**
-   * Get all Users
-   *
-   * @return List of Users
-   * @see User
-   * @see UserRepository
-   * @since 1.0
-   * @deprecated Debugging only
-   */
-  public @NotNull Iterable<User> getAllUsers() {
-    return UserRepository.findAll();
-  }
-
-  /**
-   * Find User by Username Containing a String
-   *
-   * @param name String to search for in Username
-   * @return List of Users
-   * @see User
-   * @see UserRepository
-   * @since 1.0
-   */
-  public Iterable<User> getAllByUsernameContaining(String name) {
-    return UserRepository.findAllByUsernameContaining(name);
-  }
-
-  /**
-   * Find 20 Users by Username containing a String
-   *
-   * @param username Username to search for
-   * @return list of User
-   * @see User
-   * @see UserRepository
-   * @since 1.0
-   */
-  public List<User> find20ByUsernameContaining(String username) {
-    return UserRepository.findTop20ByUsernameContainingIgnoreCaseOrderByUsernameDesc(username);
+  public List<User> search(String username ,int page) {
+    return UserRepository.search(username, PageRequest.of(page, PAGE_SIZE));
   }
 }
