2a92ad9ba667c508296b04df7377922279c85e09
schillD <davis.schill04@gmail.com>
2023-01-29 20:03:19 +0100

    New Frontend without auth

--- src/main/java/de/ghse/forum/config/WebMvcConfig.java
+++ src/main/java/de/ghse/forum/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("http://127.0.0.1:5173/")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowCredentials(true);
   }
