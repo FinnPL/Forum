b2f8e247c93067d3cc397baa2c49badfbe786b2e
schillD <davis.schill04@gmail.com>
2023-02-28 20:01:20 +0100

    Revert "Working frontend with auth"
    This reverts commit 2a9f8fe86bc148a539a724c5331b256d36afdc98.

--- board/src/lib/functions.js
+++
@@ -1,8 +0,0 @@
-export function getCookie(cookieName) {
-  let cookie = {};
-  document.cookie.split(";").forEach(function (el) {
-    let [key, value] = el.split("=");
-    cookie[key.trim()] = value;
-  });
-  return cookie[cookieName];
-}
