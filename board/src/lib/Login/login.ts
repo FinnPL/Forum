2a9f8fe86bc148a539a724c5331b256d36afdc98
schillD <davis.schill04@gmail.com>
2023-02-28 19:58:24 +0100

    Working frontend with auth

---
+++ board/src/lib/Login/login.ts
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store";
+export const token: any = writable(undefined);
+export const cookie_name: any = writable(undefined);
+export const own_user_id: any = writable(undefined);
