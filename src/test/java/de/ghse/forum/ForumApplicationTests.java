a5d61fe048fbca4b5e0d88a33da558815776c725
finnp <contact@lippok.eu>
2023-03-15 20:40:06 +0100

    added Test

--- src/test/java/de/ghse/forum/ForumApplicationTests.java
+++ src/test/java/de/ghse/forum/ForumApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ForumApplicationTests {
 
-  @Test
-  void contextLoads() {}
+        @Test
+        void contextLoads() {
+        }
 }
