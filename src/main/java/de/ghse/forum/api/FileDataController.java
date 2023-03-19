85a29217fdbdfda060c27aed590658650c7f5094
finnp <contact@lippok.eu>
2023-03-19 17:46:57 +0100

    Post files

--- src/main/java/de/ghse/forum/api/FileDataController.java
+++ src/main/java/de/ghse/forum/api/FileDataController.java
@@ -1,7 +1,9 @@
 package de.ghse.forum.api;
 
+import de.ghse.forum.service.PostService;
 import de.ghse.forum.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,25 +29,13 @@ import java.util.UUID;
 public class FileDataController {
 
     private final UserService userService;
+    private final PostService postService;
 
     Logger logger = LoggerFactory.getLogger(CommentController.class);
 
 
     @Value("${file.directory}")
     private String directory;
-
-    @PostMapping("/api/v1/file")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") List<MultipartFile> files) throws IOException {
-        logger.info("File upload request received");
-
-        if (createDirectory()) return ResponseEntity.badRequest().body("Could not create directory");
-
-        for (MultipartFile file : files) {
-            file.transferTo(Paths.get(directory, System.currentTimeMillis() + "_" + file.getOriginalFilename()));
-        }
-        return ResponseEntity.ok().body("File(s) uploaded successfully");
-
-    }
 
     @PostMapping("/api/v1/file/profile")
     public ResponseEntity<String> uploadProfilePicture(@RequestParam("file") MultipartFile file, Principal principal) throws IOException {
@@ -55,18 +45,61 @@ public class FileDataController {
 
         if (createDirectory()) return ResponseEntity.badRequest().body("Could not create directory");
         String id = userService.findbyUsername(principal.getName()).orElseThrow().getId().toString();
-        File[] files = new File(directory).listFiles((dir, name) -> name.startsWith(id));
+        return getStringResponseEntity(id, file);
 
+    }
+
+    @GetMapping("/api/v1/file/profile/{id}")
+    public ResponseEntity<ByteArrayResource> loadProfilePicture(@PathVariable("id") String id) throws IOException {
+        return getByteArrayResourceResponseEntity(id);
+    }
+
+    @PostMapping("/api/v1/file/post/{id}")
+    public ResponseEntity<String> uploadFile(@PathVariable("id") String id, @RequestParam("file") MultipartFile file,Principal principal) throws IOException {
+        logger.info("Uploading files to Post with id: " + id);
+        if (createDirectory()) return ResponseEntity.badRequest().body("Could not create directory");
+        if(postService.getPostById(UUID.fromString(id)).orElseThrow().getUser().getId().toString().equals(userService.findbyUsername(principal.getName()).orElseThrow().getId().toString())){
+            if (file.getContentType() != null && !file.getContentType().startsWith("image/")){
+                return ResponseEntity.badRequest().body("File type not supported");
+            }
+            return getStringResponseEntity(id, file);
+        }
+        return ResponseEntity.badRequest().body("You are not allowed to upload files to this post");
+    }
+
+    @NotNull
+    private ResponseEntity<String> getStringResponseEntity(@PathVariable("id") String id, @RequestParam("file") MultipartFile file) throws IOException {
+        File[] files = new File(directory).listFiles((dir, name) -> name.startsWith(id));
         if(files != null && files.length >= 1) {
             for (File f : files) {
                 if(!f.delete()) logger.error("Could not delete file: " + f.getName());
             }
         }
-
         file.transferTo(Paths.get(directory, id +"-" + UUID.randomUUID() + "."+file.getContentType().split("/")[1].toLowerCase()));
-
         return ResponseEntity.ok().body("File(s) uploaded successfully");
+    }
 
+    @GetMapping("/api/v1/file/post/{id}")
+    public ResponseEntity<ByteArrayResource> loadFile(@PathVariable String id) throws IOException {
+        return getByteArrayResourceResponseEntity(id);
+    }
+
+    @NotNull
+    private ResponseEntity<ByteArrayResource> getByteArrayResourceResponseEntity(@PathVariable String id) throws IOException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        File[] files = new File(directory).listFiles((dir, name) -> name.startsWith(id));
+        if (files == null || files.length != 1) return ResponseEntity.notFound().build();
+        httpHeaders.setContentType(MediaType.parseMediaType(Files.probeContentType(Path.of(files[0].getAbsolutePath()))));
+        httpHeaders.setContentDisposition(ContentDisposition.parse(ContentDisposition.attachment().filename(files[0].getName()).build().toString()));
+        httpHeaders.setCacheControl("max-age=36000");
+        return ResponseEntity.ok().headers(httpHeaders).body(new ByteArrayResource(Files.readAllBytes(Paths.get(files[0].getAbsolutePath()))));
+    }
+
+
+    @DeleteMapping("/api/v1/file")
+    public ResponseEntity<String> deleteFile(@RequestParam("file") String file) throws IOException {
+        Files.delete(Paths.get(directory, file));
+        return ResponseEntity.ok().body("File deleted successfully");
     }
 
     public boolean createDirectory() {
@@ -75,36 +108,5 @@ public class FileDataController {
         if (!directoryExists) directoryExists = new File(directory).mkdir();
 
         return !directoryExists;
-    }
-
-    @CrossOrigin(origins = "http://localhost:8080")
-    @GetMapping("/api/v1/file/profile/{id}")
-    public ResponseEntity<ByteArrayResource> loadProfilePicture(@PathVariable("id") String id) throws IOException {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        File[] files = new File(directory).listFiles((dir, name) -> name.startsWith(id));
-        if (files == null || files.length != 1) return ResponseEntity.notFound().build();
-        httpHeaders.setContentType(MediaType.parseMediaType(Files.probeContentType(Path.of(files[0].getAbsolutePath()))));
-        httpHeaders.setContentDisposition(ContentDisposition.parse(ContentDisposition.attachment().filename(files[0].getName()).build().toString()));
-        httpHeaders.setCacheControl("max-age=36000");
-
-        return ResponseEntity.ok().headers(httpHeaders).body(new ByteArrayResource(Files.readAllBytes(Paths.get(files[0].getAbsolutePath()))));
-    }
-
-    @CrossOrigin(origins = "http://localhost:8080")
-    @GetMapping("/api/v1/file")
-    public ResponseEntity<ByteArrayResource> loadFile(@RequestParam("file") String file) throws IOException {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.parseMediaType(Files.probeContentType(Path.of(directory + file))));
-        httpHeaders.setContentDisposition(ContentDisposition.parse(ContentDisposition.attachment().filename(file).build().toString()));
-        httpHeaders.setCacheControl("max-age=36000");
-
-        return ResponseEntity.ok().headers(httpHeaders).body(new ByteArrayResource(Files.readAllBytes(Paths.get(directory, file))));
-    }
-
-
-    @DeleteMapping("/api/v1/file")
-    public ResponseEntity<String> deleteFile(@RequestParam("file") String file) throws IOException {
-        Files.delete(Paths.get(directory, file));
-        return ResponseEntity.ok().body("File deleted successfully");
     }
 }
