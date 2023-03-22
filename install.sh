ce8312a8305c84ab62b1f6f31aeb1fc14febafbb
finnp <contact@lippok.eu>
2023-03-22 18:30:39 +0100

    fix

--- install.sh
+++ install.sh
@@ -8,21 +8,9 @@ sudo apt-get upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-# Check if Docker is installed
-if ! [ -x "$(command -v docker)" ]; then
-  echo 'Error: Docker is not installed.' >&2
-  exit 1
-fi
-
 # Install Docker Compose
 sudo apt-get update
 sudo apt-get install -y docker-compose
-
-# Check if Docker Compose is installed
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: Docker Compose is not installed.' >&2
-  exit 1
-fi
 
 # Set environment variables
 export DB_PASSWORD=$1
