4723f3ae3b16eed5d06cc886872374c2e7de3148
finnp <contact@lippok.eu>
2023-05-16 20:21:48 +0200

    optimization

--- install.sh
+++ install.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
+set -euo pipefail
 
-# Update the system
-sudo apt-get update
-sudo apt-get upgrade -y
+# Update the system and install Docker and Docker Compose
+curl -fsSL https://get.docker.com -o get-docker.sh | sh && \
+apt-get install -y docker-compose
 
-# Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-# Install Docker Compose
-sudo apt-get update
-sudo apt-get install -y docker-compose
-
-# find out the public IP address of the server
+# Find out the public IP address of the server
 export SERVER_IP=$(curl -s http://checkip.amazonaws.com)
 
 # Set environment variables
-export DB_PASSWORD=$1
-export DB_NAME=$2
-export GF_SECURITY_ADMIN_PASSWORD=$3
+export DB_PASSWORD=$1 DB_NAME=$2 GF_SECURITY_ADMIN_PASSWORD=$3
 
 # Make the gradlew file executable
 chmod +x gradlew
-# Run Docker Compose
+
+# Start Docker Compose
 docker-compose up -d
