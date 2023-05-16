72a6b63f8e2db08f285a1a62575f88449e5c68a3
finnp <contact@lippok.eu>
2023-05-16 20:53:27 +0200

    improvements to install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,24 @@ sudo apt-get install -y docker-compose
 export SERVER_IP=$(curl -s http://checkip.amazonaws.com)
 
 # Set environment variables
-export DB_PASSWORD=$1 DB_NAME=$2 GF_SECURITY_ADMIN_PASSWORD=$3
+if [ $# -eq 0 ]; then
+  echo -e "\e[33mWarning: No arguments provided. Generating random password.\e[0m"
+  export DB_PASSWORD=$(openssl rand -base64 12)
+  export DB_NAME="forum"
+  export GF_SECURITY_ADMIN_PASSWORD=$(openssl rand -base64 12)
+else
+  export DB_PASSWORD=${1} DB_NAME=${2} GF_SECURITY_ADMIN_PASSWORD=${3}
+fi
 
 # Make the gradlew file executable
 chmod +x gradlew
 
 # Start Docker Compose
 docker-compose up -d
+
+echo -e "\e[32mScript completed successfully.\e[0m"
+
+if [ $# -eq 0 ]; then
+  echo -e "\e[36mGenerated password: ${DB_PASSWORD}\e[0m"
+  echo -e "\e[36mGrafana admin password: ${GF_SECURITY_ADMIN_PASSWORD}\e[0m"
+fi
