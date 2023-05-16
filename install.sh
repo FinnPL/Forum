62522e8dff944a979600928f07c0fada22391054
finnp <contact@lippok.eu>
2023-05-16 20:25:33 +0200

    requires root privileges

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Update the system and install Docker and Docker Compose
 curl -fsSL https://get.docker.com -o get-docker.sh | sh && \
-apt-get install -y docker-compose
+sudo apt-get install -y docker-compose
 
 # Find out the public IP address of the server
 export SERVER_IP=$(curl -s http://checkip.amazonaws.com)
