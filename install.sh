689659a403b394a5032df812fdefe34d5916da89
finnp <contact@lippok.eu>
2023-05-16 21:37:53 +0200

    added --rebuild and other impro.

--- install.sh
+++ install.sh
@@ -2,30 +2,45 @@
 set -euo pipefail
 
 # Update the system and install Docker and Docker Compose
-curl -fsSL https://get.docker.com -o get-docker.sh | sh && \
-sudo apt-get install -y docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh | sh && sudo apt-get install -y docker-compose
 
 # Find out the public IP address of the server
 export SERVER_IP=$(curl -s http://checkip.amazonaws.com)
 
 # Set environment variables
-if [ $# -eq 0 ]; then
-  echo -e "\e[33mWarning: No arguments provided. Generating random password.\e[0m"
-  export DB_PASSWORD=$(openssl rand -base64 12)
-  export DB_NAME="forum"
-  export GF_SECURITY_ADMIN_PASSWORD=$(openssl rand -base64 12)
+if [[ "${@}" =~ "--rebuild" ]]; then
+  # Check if the rebuild argument is provided
+  # Stop and remove the Docker containers
+  docker-compose down
+
+  # Build and start the Docker containers
+  docker-compose up -d --build
 else
-  export DB_PASSWORD=${1} DB_NAME=${2} GF_SECURITY_ADMIN_PASSWORD=${3}
+  # Set default values for environment variables
+  DB_PASSWORD=$(openssl rand -base64 12)
+  DB_NAME="forum"
+  GF_SECURITY_ADMIN_PASSWORD=$(openssl rand -base64 12)
+
+  # Override default values with command line arguments
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --db-password) DB_PASSWORD="$2"; shift 2;;
+      --db-name) DB_NAME="$2"; shift 2;;
+      --admin-password) GF_SECURITY_ADMIN_PASSWORD="$2"; shift 2;;
+      *) echo "Invalid argument: $1"; exit 1;;
+    esac
+  done
+
+  # Make the gradlew file executable
+  chmod +x gradlew
+
+  # Start the Docker containers
+  docker-compose up -d
 fi
-
-# Make the gradlew file executable
-chmod +x gradlew
-
-# Start Docker Compose
-docker-compose up -d
 
 echo -e "\e[32mScript completed successfully.\e[0m"
 
+# Print generated passwords if no arguments were provided
 if [ $# -eq 0 ]; then
   echo -e "\e[36mGenerated password: ${DB_PASSWORD}\e[0m"
   echo -e "\e[36mGrafana admin password: ${GF_SECURITY_ADMIN_PASSWORD}\e[0m"
