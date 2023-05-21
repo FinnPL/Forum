25aca1ffa477c3af689d8a277d589af072a1ec6c
finnp <contact@lippok.eu>
2023-05-21 19:13:10 +0200

    fix2

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# Update the system and install Docker and Docker Compose
-curl -fsSL https://get.docker.com -o get-docker.sh | sh && \
-sudo apt-get install -y docker-compose
+# Install Docker and Docker Compose using the package manager
+sudo apt-get update && sudo apt-get install -y docker.io docker-compose
 
 # Find out the public IP address of the server
 export SERVER_IP=$(curl -s http://checkip.amazonaws.com)
