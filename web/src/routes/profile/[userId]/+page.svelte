8464fae936686b4f7843f6063f9d0b6aeff26595
schillD <davis.schill04@gmail.com>
2023-07-03 20:37:33 +0200

    stores rework

--- web/src/routes/profile/[userId]/+page.svelte
+++ web/src/routes/profile/[userId]/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
   import { fetchPage, fetcher, getCookie } from "../../../lib/functions";
   import { onMount } from "svelte";
-  import { own_user_id, token } from "../../../lib/Login/login";
   import { goto } from "$app/navigation";
   import PostItem from "$lib/PostItem.svelte";
   import { default as defaultAvatar } from "../../../lib/assets/defaultAvatar.png";
+  import { store_token, store_userid } from "$lib/stores";
   export let data: any;
 
   let postList: any = [];
   let page = 0;
-  let user_name: string;
   let bio: string;
   let bio_update: string;
-  let tokenValue: string;
   let file: any;
-  let own_user_id_value: string;
   let avatarSrc: any = null;
+  let user_name: string;
   let avatar_file: any;
   //Modal
   let open = false;
@@ -28,26 +26,16 @@
   }
 
   async function checkLoggedIn() {
-    tokenValue = await getCookie("tokenValue");
-    token.set(tokenValue);
+    $store_token = await getCookie("tokenValue");
+    $store_userid = await getCookie("userid");
 
-    own_user_id_value = await getCookie("userid");
-    own_user_id.set(own_user_id_value);
   }
 
-  async function subStores() {
-    token.subscribe((value: string) => {
-      tokenValue = value;
-    });
-    own_user_id.subscribe((value: string) => {
-      own_user_id_value = value;
-    });
-  }
+  
 
   onMount(async () => {
     await get_server_ip();
     await checkLoggedIn();
-    await subStores();
     console.log(avatarSrc);
     getUserDetails();
     getPostList();
@@ -56,7 +44,7 @@
   async function getUserDetails() {
     const fetchedDataRes: any = await fetch(ip + "api/v1/user/" + data.userId, {
       method: "GET",
-      headers: { Authorization: "Bearer " + tokenValue },
+      headers: { Authorization: "Bearer " + $store_token },
     });
     if (!fetchedDataRes.ok) {
       await goto("/");
@@ -103,12 +91,12 @@
       method: "POST",
       body: formData,
       headers: {
-        Authorization: "Bearer " + tokenValue,
+        Authorization: "Bearer " + $store_token,
       },
     });
     console.log(res);
     await goto("/");
-    await goto("/profile/" + own_user_id_value);
+    await goto("/profile/" + $store_userid);
     location.reload();
   }
 
@@ -135,9 +123,9 @@
         if (event.target && event.target.result) {
           avatarSrc = event.target.result;
         }
+        console.log(avatarSrc);
       };
     }
-    
     loadAvatar();
     console.log(avatarSrc);
   });
@@ -171,47 +159,50 @@
     {/if}
 
     <div class="pl-3">
-      <span class="font-semibold">{user_name}</span>
+    <span class="font-semibold">{user_name}</span>
 
-      {#if bio != null && bio != "null"}
-        <p class="pt-1 mr-1 break-words">{bio}</p>
-      {/if}
-    </div>
-
-    {#if own_user_id_value == data.userId}
-      <div class="flex-1 flex justify-end">
-        <button class="hover:bg-hover rounded" on:click={toggle}>
-          <svg fill="#ffffff" height="50px" width="50px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="-306.64 -306.64 919.92 919.92" xml:space="preserve">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>    
-            <g id="SVGRepo_iconCarrier"> <g> <g> <path d="M12.809,238.52L0,306.637l68.118-12.809l184.277-184.277l-55.309-55.309L12.809,238.52z M60.79,279.943l-41.992,7.896 l7.896-41.992L197.086,75.455l34.096,34.096L60.79,279.943z"/> <path d="M251.329,0l-41.507,41.507l55.308,55.308l41.507-41.507L251.329,0z M231.035,41.507l20.294-20.294l34.095,34.095 L265.13,75.602L231.035,41.507z"/> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g> 
-          </svg>
-        </button>
-      </div>
+    {#if bio != null && bio != "null"}
+      <p class="pt-1 mr-1 break-words">{bio}</p>
     {/if}
   </div>
 
-  <div class={open ? "block pt-5" : "hidden"}>
-    <div class="bg-postBG border border-border p-4 rounded-lg max-w-5xl">
-      <div class="text-lg font-semibold mb-2">Beschreibung bearbeiten:</div>
-      <div class="mb-6">
-        <textarea class="text-white bg-ui border border-border rounded-lg w-full resize-none" maxlength="255" bind:value={bio_update}/>
-      </div>
+    {#if $store_userid == data.userId}
+    <div class="flex-1 flex justify-end">
+      <button class="hover:bg-hover rounded" on:click={toggle}>
+        <svg fill="#ffffff" height="50px" width="50px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="-306.64 -306.64 919.92 919.92" xml:space="preserve">
+          <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>    
+          <g id="SVGRepo_iconCarrier"> <g> <g> <path d="M12.809,238.52L0,306.637l68.118-12.809l184.277-184.277l-55.309-55.309L12.809,238.52z M60.79,279.943l-41.992,7.896 l7.896-41.992L197.086,75.455l34.096,34.096L60.79,279.943z"/> <path d="M251.329,0l-41.507,41.507l55.308,55.308l41.507-41.507L251.329,0z M231.035,41.507l20.294-20.294l34.095,34.095 L265.13,75.602L231.035,41.507z"/> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g> 
+        </svg>
+      </button>
+    </div>
 
-      <hr class="h-0.5 border-t-0 bg-text" />
+    {/if}
+  
+</div>
 
-      <div class="text-lg font-semibold pt-3 mb-2">Profilbild bearbeiten:</div>
-      <div class="mb-4">
-        <input type="file" name="file" id="AvatarFile" bind:this={avatar_file} on:change={handleFileChange}/>
-      </div>
+<div class={open ? "block pt-5" : "hidden"}>
+  <div class="bg-postBG border border-border p-4 rounded-lg max-w-5xl">
+    <div class="text-lg font-semibold mb-2">Beschreibung bearbeiten:</div>
+    <div class="mb-6">
+      <textarea class="text-white bg-ui border border-border rounded-lg w-full resize-none" maxlength="255" bind:value={bio_update}/>
+    </div>
 
-      <div class="flex justify-end">
-        <button class="bg-red-500 hover:bg-hover text-white px-4 py-2 rounded" on:click={toggle}>Abbrechen</button>
-        <button class="bg-primary hover:brightness-75 text-white px-4 py-2 rounded ml-2" on:click={toggle} on:click={update_bio} on:click={upload_avatar}>Update Profile</button>
-      </div>
+    <hr class="h-0.5 border-t-0 bg-text" />
+
+    <div class="text-lg font-semibold pt-3 mb-2">Profilbild bearbeiten:</div>
+    <div class="mb-4">
+      <input type="file" name="file" id="AvatarFile" bind:this={avatar_file} on:change={handleFileChange}/>
+    </div>
+
+    <div class="flex justify-end">
+      <button class="bg-red-500 hover:bg-hover text-white px-4 py-2 rounded" on:click={toggle}>Abbrechen</button>
+      <button class="bg-primary hover:brightness-75 text-white px-4 py-2 rounded ml-2" on:click={toggle} on:click={update_bio} on:click={upload_avatar}>Update Profile</button>
     </div>
   </div>
 </div>
+</div>
+
 
 {#if postList[0] != undefined}
   {#each postList as post (post.id)}
