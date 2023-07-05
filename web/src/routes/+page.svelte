53bf9e5e5e2b71a63677c0b5db7b1bc516a2a5aa
schillD <davis.schill04@gmail.com>
2023-07-05 17:57:28 +0200

    quick fix for previous commit

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,16 +2,17 @@
   import Login from "../lib/Login/login.svelte";
   import Post_List from "./post_list/+page.svelte";
   import { store_token } from "$lib/stores";
+  import { getCookie } from "$lib/functions";
 
-  let promise = (async () => {
-    return $store_token;
-  })
+  async function getPromise() {
+    return await getCookie("tokenValue");
+  }
 
-  
+  let promise = getPromise();
 </script>
 
 {#await promise then token}
-    {#if token !== undefined}
+    {#if token != undefined}
       <Post_List />
       {:else}
         <Login show_sign_up="false" />
