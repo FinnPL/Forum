931114ad81d291a5554538796113e18c8227346e
finnp <contact@lippok.eu>
2023-07-05 22:33:04 +0200

    Changed timezone of Jackson to Central European Time

--- server/src/main/java/de/ghse/forum/model/Post.java
+++ server/src/main/java/de/ghse/forum/model/Post.java
@@ -43,7 +43,7 @@ public class Post {
   @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
   private Collection<Comment> comment;
 
-  private final java.sql.Timestamp date = new Timestamp(System.currentTimeMillis());
+  @Builder.Default private final Timestamp date = new Timestamp(System.currentTimeMillis());
 
   @Builder.Default private boolean edited = false;
 }
